07f2b220e403829c7ec8131d3ce805ec294bbc24
DennisN20 <114265224+DennisN20@users.noreply.github.com>
2022-10-20 17:22:15 +0100

    Basic_HTML_1

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello World! I am an alertbox!");
+
+function My_First_Function(){
+var String = "Kiss me, i'm Irish!";
+var result = String.fontcolor("green");
+document.getElementById("Irish").innerHTML = 
+result;
+}
